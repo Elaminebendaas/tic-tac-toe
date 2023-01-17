ba39bbdc2898a410aedac85baf7d8d3f3c326cdc
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-01-17 11:33:30 -0500

    boiler for js

---
+++ app.js
@@ -0,0 +1,19 @@
+const GameBoard = () =>{
+    let gameBoard = [[,,],[,,],[,,]]
+    const posAddition = (x, y, symbol) =>{
+
+    }
+    return{gameBoard, posAddition}
+} 
+
+const Player = (symbol) =>{
+    let playerSymbol = symbol
+    return{playerSymbol}
+}
+
+const Game = (player) =>{
+
+}
+
+
+
